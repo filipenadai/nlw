996a8ac7715bc20bbde99ec76fee327ce79fa514
Filipe Nadai <f.nadai05@gmail.com>
2020-06-03 13:22:42 -0300

    editions

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,33 +1,9 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
-const users = [
-    'Filipe',
-    'Karina',
-    'Isabela'
-];
-
-app.get('/user', (req, res) => {
-    console.log('Listar usuários');
-
-    res.json(users);
-});
-
-app.get('/user/:id', (req, res) => {
-    const id = Number(req.params.id);
-
-    const user = users[id];
-
-    return res.json(user);
-})
-
-app.post('/user', (req, res) => {
-    const users = {
-        nome : 'Filipe',
-        email : 'f.nadai05@gmail.com'
-    };
-    return res.json(users);
-});
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333);
