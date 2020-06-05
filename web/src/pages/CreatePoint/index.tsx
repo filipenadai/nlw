d53a44f58b36b1f67098232f45bbbfaee7515d1d
Filipe Nadai <f.nadai05@gmail.com>
2020-06-04 21:05:48 -0300

    routes and pages

---
+++ web/src/pages/CreatePoint/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './stles.css';
+
+import logo from '../../assets/logo.svg';
+
+const CreatePoint = () => {
+    return (
+        <div id="page-create-point">
+            <header>
+                <img src={logo} alt="Ecoleta"/>
+            </header>
+        </div>
+    );
+}
+
+export default CreatePoint;
