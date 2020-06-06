39664f93b2b850b95a91e340c3e3b18b0af14acf
Filipe Nadai <f.nadai05@gmail.com>
2020-06-06 08:26:02 -0300

    mapa, uf

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,12 +1,42 @@
-import React from 'react';
+import React, {useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
-
+import { Map, TileLayer, Marker } from 'react-leaflet';
 import './stles.css';
+import axios from 'axios';
+import api from '../../services/api';
 
 import logo from '../../assets/logo.svg';
 
+interface Items {
+    id: number;
+    title: string;
+    image_url: string;
+}
+
+interface UFIBGEResponse {
+    sigla: string;
+}
+
 const CreatePoint = () => {
+
+    const [items, setItems] = useState<Items[]>([]);
+    const [ufs, setUfs] = useState<string[]>([]);
+
+    useEffect(()=> {
+        api.get('items').then(response => {
+            setItems(response.data);
+        });
+    }, []);
+
+    useEffect(()=>{
+        axios.get<UFIBGEResponse[]>('https://servicodados.ibge.gov.br/api/v1/localidades/estados').then(response =>{
+        const ufInitials = response.data.map(uf => uf.sigla);
+
+        setUfs(ufInitials);
+        });
+    }, []);
+
     return (
         <div id="page-create-point">
             <header>
@@ -61,10 +91,25 @@ const CreatePoint = () => {
                         <span>Selecione o endereço no mapa</span>
                     </legend>
 
+                        <Map center={[-22.3909612, -47.5570119]} zoom={15}>
+                            <TileLayer
+                                attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                            />
+
+                            <Marker position={[-22.3909612, -47.5570119]}/>
+                        </Map>
+
                     <div className="field-group">
                         <div className="field">
                             <label htmlFor="uf">UF</label>
-                            <select name="uf" id="uf"></select>
+                            <select name="uf" id="uf">
+                                <option value="0">Selecione uma UF</option>
+                                {ufs.map(uf => (
+                                 <option key={uf} value={uf}>{uf}</option>
+                                ))}
+                            </select>
+                            
                         </div>
 
                         <div className="field">
@@ -81,30 +126,14 @@ const CreatePoint = () => {
                     </legend>
 
                     <ul className="items-grid">
-                        <li>
-                            <img src="http://localhost:3333/uploads/oleo.svg" alt="Teste"/>
-                            <span>Óleo de cozinha</span>
-                        </li>
-                        <li>
-                            <img src="http://localhost:3333/uploads/oleo.svg" alt="Teste"/>
-                            <span>Óleo de cozinha</span>
-                        </li>
-                        <li>
-                            <img src="http://localhost:3333/uploads/oleo.svg" alt="Teste"/>
-                            <span>Óleo de cozinha</span>
-                        </li>
-                        <li>
-                            <img src="http://localhost:3333/uploads/oleo.svg" alt="Teste"/>
-                            <span>Óleo de cozinha</span>
-                        </li>
-                        <li>
-                            <img src="http://localhost:3333/uploads/oleo.svg" alt="Teste"/>
-                            <span>Óleo de cozinha</span>
-                        </li>
-                        <li>
-                            <img src="http://localhost:3333/uploads/oleo.svg" alt="Teste"/>
-                            <span>Óleo de cozinha</span>
-                        </li>
+
+                        {items.map(item => (
+                            <li key={item.id}>
+                                <img src={item.image_url}alt={item.title}/>
+                                <span>{item.title}</span>
+                            </li>
+                        ))}
+
                     </ul>
                 </fieldset>
 
